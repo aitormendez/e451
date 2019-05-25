24be6f37e7e35dd37b22480a909cb6729d959bde
aitor <aitor@e451.net>
2019-05-25 15:38:14 +0200

    GSAP

--- web/app/themes/451/resources/assets/scripts/routes/common.js
+++ web/app/themes/451/resources/assets/scripts/routes/common.js
@@ -1,3 +1,12 @@
+/* eslint-disable no-unused-vars */
+import {
+  TweenMax,
+  TimelineLite,
+  Power2,
+  Elastic,
+  CSSPlugin,
+} from 'gsap/TweenMax';
+
 export default {
   init() {
     // JavaScript to be fired on all pages
